eb11549ec67e9bc345f2b05a1b8b68e9e15b9943
EquipoBascula <156357227+EquipoBascula@users.noreply.github.com>
2024-01-11 17:34:49 -0600

    Update EnvioDatosFormulario.dart

--- formulario/lib/supabase/EnvioDatos/EnvioDatosFormulario.dart
+++ formulario/lib/supabase/EnvioDatos/EnvioDatosFormulario.dart
@@ -2,7 +2,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:formulario/supabase/Conexion/SupabaseService.dart';
 import 'package:formulario/pantallas/funciones/Controladores_Texto.dart';
 import 'package:supabase/supabase.dart';
-
+//Esta clase contiene tres métodos que se utilizan para enviar datos de un 
+//formulario a una base de datos de Supabase.
 class EnvioDatosFormularioPrincipal {
   final FormData formData = FormData(); // Instancia del Singleton
   static final SupabaseClient _supabaseClient = SupabaseService.supabaseClient;
@@ -17,6 +18,9 @@ class EnvioDatosFormularioPrincipal {
     var fecha_nac = formData.fecha_C.text;
     var id_usuario1 = 1; //Es temporal el pasar el id del usuario manualmente
 
+    //Estas líneas de código utilizan la biblioteca supabase_flutter para
+    //realizar una consulta a la tabla informacion_basica_usuario de la base de datos de Supabase. 
+    //La consulta inserta los datos personales del usuario en la tabla.
     var response = await _supabaseClient
         .from('informacion_basica_usuario') // nombre de la tabla
         .insert({
@@ -28,7 +32,8 @@ class EnvioDatosFormularioPrincipal {
       'nacionalidad': pais,
       'id_usuario': id_usuario1
     });
-
+//este código se utiliza para comprobar si la consulta a la base de datos 
+    //se realizó correctamente. Si la consulta se realizó correctamente, el código imprime un mensaje de éxito. Si la consulta no se realizó correctamente, el código imprime un mensaje de error
     if (response != null) {
       if (response.error != null) {
         print("Error al enviar datos: ${response.error!.message}");
@@ -47,6 +52,9 @@ class EnvioDatosFormularioPrincipal {
     var hayAlergias = formData.hayAlergias;
     var id_usuario1 = 1; //Es temporal el pasar el id del usuario manualmento
 
+    //Estas líneas de código utilizan la biblioteca supabase_flutter para realizar 
+    //una consulta a la tabla informacion_especial_usuario de la base de datos de Supabase. 
+    //La consulta inserta los datos físicos del usuario en la tabla.
     var response =
         await _supabaseClient.from('informacion_especial_usuario').insert({
       'discapacidad': discapacidad,
@@ -56,6 +64,8 @@ class EnvioDatosFormularioPrincipal {
       'alergias': hayAlergias,
       'id_usuario': id_usuario1
     });
+//este código se utiliza para comprobar si la consulta a la base de datos 
+    //se realizó correctamente. Si la consulta se realizó correctamente, el código imprime un mensaje de éxito. Si la consulta no se realizó correctamente, el código imprime un mensaje de error
 
     if (response != null) {
       if (response.error != null) {
@@ -79,6 +89,8 @@ class EnvioDatosFormularioPrincipal {
     var otroTipo = formData.otroAlimento_C.text;
     var id_usuario1 = 1; //Es temporal el pasar el id del usuario manualmento
 
+    //Estas líneas de código asignan a variables los valores de los campos del 
+    //formulario que contienen los datos de alergias del usuario.
     var response = await _supabaseClient.from('alergias_usuario').insert({
       'lacteos': lacteos,
       'trigo': trigo,
@@ -91,7 +103,8 @@ class EnvioDatosFormularioPrincipal {
       'otro_tipo': otroTipo,
       'id_usuario': id_usuario1
     });
-
+//este código se utiliza para comprobar si la consulta a la base de datos 
+    //se realizó correctamente. Si la consulta se realizó correctamente, el código imprime un mensaje de éxito. Si la consulta no se realizó correctamente, el código imprime un mensaje de error
     if (response != null) {
       if (response.error != null) {
         print("Error al enviar datos: ${response.error!.message}");
