6a9c3563031240515271344ba3684494991a760f
EquipoBascula <156357227+EquipoBascula@users.noreply.github.com>
2024-01-11 17:47:10 -0600

    Update main.dart

--- formulario/lib/main.dart
+++ formulario/lib/main.dart
@@ -6,18 +6,27 @@ import 'package:formulario/pantallas/tipo.dart';
 import  'pantallas/inicio_bascula/diagrama_circular.dart';
 import  'pantallas/inicio_bascula/diagrama_lineal.dart';
 import  'pantallas/inicio_bascula/datos_diagramas.dart';
-
+//Esta función es el punto de entrada de la aplicación. Llama a la función runApp() 
+//para iniciar la aplicación.
 void main() {
   runApp(MyApp());
 }
+//La clase MyApp() representa la aplicación Flutter. Hereda de la clase StatelessWidget.
 class MyApp extends StatelessWidget {
 
   @override
+  //El método build() se utiliza para construir la interfaz de usuario de la aplicación.
   Widget build(BuildContext context) {
     return MaterialApp(
     title: 'Tipo de Usuario',
+    //La pantalla Tipo() se utiliza para mostrar la pantalla para seleccionar el tipo de usuario.
+    //Seleccionarlo sera el inicio de la aplicacion del espejo
     home: const Tipo(),
+      // Evitar que se muestra la bandera de modo de depuración.
     debugShowCheckedModeBanner: false,
+    //La propiedad theme se establece en una instancia de la clase ThemeData. 
+    //Esta clase se utiliza para configurar el tema de la aplicación. 
+    //En este caso, se establece el fondo del Scaffold en transparente.
     theme: ThemeData(
       scaffoldBackgroundColor: Colors.transparent, // Hacemos el fondo del Scaffold transparente
     ),
