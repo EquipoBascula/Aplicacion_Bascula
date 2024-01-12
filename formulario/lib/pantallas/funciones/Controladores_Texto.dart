65a6f3ca323f8be5647c783274a1553a0f432bc5
EquipoBascula <156357227+EquipoBascula@users.noreply.github.com>
2024-01-11 18:00:21 -0600

    Update Controladores_Texto.dart

--- formulario/lib/pantallas/funciones/Controladores_Texto.dart
+++ formulario/lib/pantallas/funciones/Controladores_Texto.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-
+//Almacena los controladores de texto de un formulario.
+//Implementa el patrón Singleton para garantizar que solo exista una instancia de la clase.
 // Clase Singleton para almacenar controladores de texto
 class FormData {
   static final FormData _instance = FormData._internal();
+  //Método de fábrica para obtener la única instancia de la clase.
   factory FormData() {
     return _instance;
   }
+  //Constructor privado para evitar la creación directa de instancias de la clase.
   FormData._internal();
-
+  
   //Fomulario parte 1
   TextEditingController nombre_C = TextEditingController();
   TextEditingController fecha_C = TextEditingController();
@@ -27,7 +30,9 @@ class FormData {
 
   //Fomulario parte 4
   TextEditingController otroAlimento_C = TextEditingController(text: '');
+  //Indica si el usuario tiene alergias alimentarias.
   bool hayAlergias = false;
+  //Indican las alergias específicas del usuario.
   bool aLacteos = false;
   bool aTrigo = false;
   bool aSoya = false;
@@ -37,6 +42,7 @@ class FormData {
   bool aMariscos = false;
   bool aHuevos = false;
 
+  //Se utilizan para guardar las opciones seleccionadas por el usuario en el formulario.
   void sethayAlergias(bool value) {
     //Establecer el valor dependiendo de la eleccion
     hayAlergias = value;
@@ -48,30 +54,37 @@ class FormData {
   }
 
   void setaTrigo(bool value) {
+    //Establecer el valor dependiendo de la eleccion
     aTrigo = value;
   }
 
   void setaSoya(bool value) {
+    //Establecer el valor dependiendo de la eleccion
     aSoya = value;
   }
 
   void setaPescado(bool value) {
+    //Establecer el valor dependiendo de la eleccion
     aPescado = value;
   }
 
   void setaFrutosSecos(bool value) {
+    //Establecer el valor dependiendo de la eleccion
     aFrutosSecos = value;
   }
 
   void setaMani(bool value) {
+    //Establecer el valor dependiendo de la eleccion
     aMani = value;
   }
 
   void setaMariscos(bool value) {
+    //Establecer el valor dependiendo de la eleccion
     aMariscos = value;
   }
 
   void setaHuevos(bool value) {
+    //Establecer el valor dependiendo de la eleccion
     aHuevos = value;
   }
 }
